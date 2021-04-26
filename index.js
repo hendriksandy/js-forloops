aec4009deb75bedf9552f161957ad8154bfc362d
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-04-26 02:11:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+for (x = 0; x < 21; x++) {
+    if (x % 2 == 0) {
+        console.log(x + " is even");
+    } else {
+        console.log(x + " is oneven ");
+    }
+}
---
+++ index2.js
@@ -0,0 +1,7 @@
+for (y = 1; y < 11; y++) {
+    for (x = 1; x < 11; x++) {
+        sum = y * x
+        console.log(y + " x " + x + " = " + sum);
+
+    }
+};
